a7cea61913615dac658a3a65afd0ff48a05471fd
wi11e <willredmondson@gmail.com>
2020-05-10 18:44:23 +0100

    feat: grab the users recent top tracks

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,31 +1,95 @@
-/**
- * Copyright 2017 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 'use strict';
 
 import * as functions from 'firebase-functions'
-
 // if you need to use the Firebase Admin SDK, uncomment the following:
 // import * as admin from 'firebase-admin'
+import axios from 'axios';
+import * as  qs from 'querystring';
 
-
-// Create and Deploy Cloud Function with TypeScript using script that is
-// defined in functions/package.json:
-//    cd functions
-//    npm run deploy
-
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send('Hello from Firebase!\n\n');
+export const helloWorld = functions.https.onRequest((req, res) => {
+ res.send('Hello from Firebase!\n\n');
 });
+
+// https://developer.spotify.com/documentation/general/guides/authorization-guide/
+// TODO put under '/' route
+export const login = functions.https.onRequest((req, res) => {
+ const scopes = 'user-read-private user-read-email user-top-read'; // TODO increase scopes
+ const clientId = 'f113a4afacf74665b6744b7406947cf8';
+ const redirectUri = 'https://us-central1-colabdiscover.cloudfunctions.net/getTopTracks';
+ res.redirect('https://accounts.spotify.com/authorize' +
+   '?response_type=code' +
+   '&client_id=' + clientId +
+   (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+   '&redirect_uri=' + encodeURIComponent(redirectUri))
+ // TODO add state
+});
+
+
+interface AuthResponse {
+ access_token: string;
+ token_type: string;
+ scope: string;
+ expires_in: number;
+ refresh_token: string;
+}
+
+type TrackResponse =  any; // TODO;
+
+ export const getTopTracks = functions.https.onRequest(async (req, res) => {
+
+  // TODO: for debug - remove
+  axios.interceptors.request.use(request => {
+   console.log('Starting Request', request);
+   return request
+  });
+  axios.interceptors.response.use(response => {
+   console.log('Response:', response);
+   return response
+  });
+
+  const code = req.query.code as string;
+  const redirectUri = 'https://us-central1-colabdiscover.cloudfunctions.net/getTopTracks';
+  const params = {
+   grant_type: 'authorization_code',
+   code,
+   redirect_uri: redirectUri
+  };
+  const config = {
+   auth: {
+    username: 'f113a4afacf74665b6744b7406947cf8',
+    password: '515fe95c0f2f47a8bc033fc37737f184'
+   },
+   headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+   }
+  };
+
+  const authResponse: {data: AuthResponse} = await axios.post(
+    'https://accounts.spotify.com/api/token',
+    qs.stringify(params),
+    config
+  );
+
+  const accessToken = authResponse.data.access_token;
+  const tracksResponse: TrackResponse = await axios.get('https://api.spotify.com/v1/me/top/tracks', {
+   headers: {
+     'Authorization': 'Bearer ' + accessToken,
+     'Content-Type': 'application/json'
+   },
+   params: {
+     limit: 20,
+     offset: 0,
+     time_range: 'short_term'
+   }
+  });
+  const tracks = tracksResponse.data.items.map((item) => {
+   return {
+    title: item.name,
+    artist: item.artists[0].name,
+    album: item.album.name
+   }
+  });
+
+  res.send(tracks);
+ });
+
